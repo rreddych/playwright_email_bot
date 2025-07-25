bee8df83cf6ef0bbc0020033b2d104ed71bb25c7
Rajesh Chinta <rajeshreddychinta@gmail.com>
2025-07-25 21:20:23 +0530

    added log messages

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from playwright.sync_api import sync_playwright
 import yagmail
+import logging
 
 def take_screenshot():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("https://duckduckgo.com/", timeout=60000)
+        logging
         screenshot_path = "screenshot.png"
         page.screenshot(path=screenshot_path)
+        logging
         browser.close()
     return screenshot_path
 
@@ -20,6 +23,7 @@ def send_email(screenshot_path):
         contents="Here is the screenshot of the website.",
         attachments=screenshot_path,
     )
+    logging
 
 if __name__ == "__main__":
     path = take_screenshot()
